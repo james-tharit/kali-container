fc684f1cc3aea775a417287b10840fe8a5f20ff1
James T <tharit.rt@gmail.com>
2025-03-03 09:13:31 +0700

    (ref): changed podman to docker

--- kaliz.sh
+++ kaliz.sh
@@ -1,8 +1,8 @@
 # build docker image from Dockerfile with tag called "kaliz"
 
-podman build -t kaliz .
+docker build -t kaliz .
 
 # random hostname for our kali instance
 hostname=$(cat words.txt | sort -R | head -1)
 # start new kaliz container in interactive mode with shell
-podman run --rm -it --network="host" -u root --cap-add=ALL --hostname=$hostname --name=$hostname kaliz  
+docker run --rm -it --network="host" --hostname=$hostname --name=$hostname kaliz  
